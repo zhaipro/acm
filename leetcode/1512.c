c7b93495be63cb6e89471692e8081ef8dd3d7412
yifuda <zhaipro@foxmail.com>
2021-03-31 20:35:06 +0800

    1512. 好数对的数目

---
+++ leetcode/1512.c
@@ -0,0 +1,13 @@
+int numIdenticalPairs(int* nums, int numsSize){
+    int r = 0;
+    for(int i=0; i<numsSize; i++)
+    {
+        for(int j=i + 1; j<numsSize; j++)
+        {
+            if(nums[i] == nums[j]) {
+                r ++;
+            }
+        }
+    }
+    return r;
+}
