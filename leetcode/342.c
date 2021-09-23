429f9531ff9de2e1c3f1c74b7c4469c92c0d3996
yifuda <zhaipro@foxmail.com>
2021-09-23 23:41:49 +0800

    342. 4的幂

--- leetcode/342.c
+++ leetcode/342.c
@@ -5,3 +5,13 @@ bool isPowerOfFour(int n){
     }
     return n == 1;
 }
+
+bool isPowerOfFour(int n){
+    if (n <= 0) return false;
+    if (n == 1) return true;
+    return (!(((n - 2) & n) | ((n - 1) & n))) && (n & 0x55555555);
+}
+
+bool isPowerOfFour(int n) {
+    return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
+}
