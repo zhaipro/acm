008cce511724d81142609cf845cad41aab5a309e
宅教授 <zhaipro@foxmail.com>
2021-12-07 16:22:40 +0800

    剑指 Offer 22. 链表中倒数第k个节点

---
+++ leetcode/offer_22.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        r = []
+        while head:
+            r.append(head)
+            head = head.next
+        return r[-k]
